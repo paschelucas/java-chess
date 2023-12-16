1c8139fed4ff0e8731f598c1f9a200865002cd63
Lucas <paschelucas9@gmail.com>
2023-12-16 00:11:25 -0300

    creates Position class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,10 @@
 package application;
 
+import boardgame.Position;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Position position = new Position(2, 4);
+        System.out.println("Olá!!" + position.toString());
     }
 }
